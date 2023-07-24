648f7397c96275f6b4b66b557ed6ea7342bc23d2
Altenia Gaines <gaines.tina90@gmail.com>
2023-07-23 17:47:08 -0700

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your code here
+
+function submitData(name, email) {
+  const configurationObject = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email,
+    }),
+  };
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((res) => res.json())
+    .then((object) => document.querySelector("body").append(object.id))
+    .catch((err) => document.querySelector("body").append(err.message));
+}
